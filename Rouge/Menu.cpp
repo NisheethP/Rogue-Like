ac3e9906f14fb9872d7575740223ef98c35f2f6b
Nisheeth <pandey.nisheeth59916031@gmail.com>
2014-11-25 22:30:11 +0530

    Added lots of functionality to the class related to how it will work, Can now show any menu on screen

--- Rouge/Menu.cpp
+++ Rouge/Menu.cpp
@@ -1,10 +1,72 @@
 #include "Menu.h"
+#include <iostream>
+using std::cout;
 
 
-Menu::Menu()
+Menu::Menu(Coord p_InitCoord, Menu* p_PrevMenu, bool p_IsTerminating) :
+initCoord(p_InitCoord),
+prevMenu(prevMenu),
+childMenu(ChildMenus()),
+commands(StringVector()),
+isTerminating(p_IsTerminating)
 {
 }
 
+void Menu::drawMenu()
+{
+	ClearScreen();
+	gotoxy(initCoord);
+	Coord tempCoord = initCoord;
+	for (int i = 0; i < commands.size(); i++)
+	{
+		gotoxy(tempCoord);
+		if (i < commands.size())
+			cout << commands[i];
+
+		tempCoord.y += 2;
+	}
+}
+
+void Menu::addCommand(std::string str)
+{
+	commands.push_back(str);
+}
+bool Menu::addChildMenu(int commandNum, Menu* menu)
+{
+	if (commandNum < commands.size())
+		childMenu[commands[commandNum]] = menu;
+	else
+		return false;
+
+	return true;
+}
+
+Menu* Menu::getMenu(std::string str)
+{
+	if (childMenu.find(str) == childMenu.end())
+		return nullptr;
+	else
+		return childMenu[str];
+}
+
+Menu* Menu::getMenu(Coord crd)
+{
+	Coord delCoord = crd - initCoord;
+	if (delCoord.y < 0 || commands.size()==0)
+		return nullptr;
+
+	int strNum = delCoord.y / 2;
+	std::string tempStr = commands[strNum];
+	if (delCoord.x <= tempStr.size())
+		return getMenu(tempStr);
+	else
+		return nullptr;
+}
+
+Menu* Menu::getPrevMenu()
+{
+	return prevMenu;
+}
 
 Menu::~Menu()
 {
--- Rouge/Menu.h
+++ Rouge/Menu.h
@@ -2,12 +2,36 @@
 #ifndef _MENU_H_
 #define _MENU_H_
 #include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include "Functions.h"
+
+class Menu;
+
+using StringVector = std::vector<std::string>;
+using ChildMenus = std::map<std::string, Menu*>;
 
 class Menu
 {
-
+protected:
+	Coord initCoord;
+	Menu* prevMenu;
+	ChildMenus childMenu;
+	StringVector commands;
 public:
-	Menu();
+	const bool isTerminating;
+	void addCommand(std::string str);
+	bool addChildMenu(int commandNum, Menu* menu);
+	
+	Menu(Coord p_InitCoord = { 0, 0 }, Menu* p_PrevMenu = nullptr, bool p_IsTerminating = false);
+	void drawMenu();
+	Menu* getMenu(std::string str);
+	Menu* getMenu(Coord crd);
+	Menu* getPrevMenu();
+
+	int getNumChild();
+
 	~Menu();
 };
 
