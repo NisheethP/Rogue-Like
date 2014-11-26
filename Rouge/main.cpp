55a89fe06ea42d07485353ec162b41ec946cfbe6
Nisheeth <pandey.nisheeth59916031@gmail.com>
2014-11-26 14:00:50 +0530

    Can now handle mouse input.

--- Rouge/main.cpp
+++ Rouge/main.cpp
@@ -1,11 +1,21 @@
 #include "Functions.h"
 #include "Menu.h"
-#include "MainMenu.h"
+#include <Windows.h>
 #include <iostream>
+
+Coord MenuMouseProc(MOUSE_EVENT_RECORD mouseEvent);
+DWORD oldWindowMode;
 
 int main()
 {
 	SetDefaultColour();
+	
+	INPUT_RECORD inputBuffer[128];
+	DWORD numInput;
+
+	GetConsoleMode(Global::hStdin, &oldWindowMode);
+	SetConsoleMode(Global::hStdin, ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);
+
 	Menu mainMenu({ 10,5 });
 	Menu newGame({ 0, 0 }, &mainMenu);
 	Menu options({ 10, 5 }, &mainMenu);
@@ -20,6 +30,7 @@ int main()
 
 	mainMenu.addChildMenu(0, &newGame);
 	mainMenu.addChildMenu(1, &options);
+	mainMenu.addChildMenu(2, &quit);
 
 	options.addChildMenu(1, &mainMenu);
 
@@ -31,9 +42,26 @@ int main()
 	bool isInMenu = true;
 	while (isInMenu)
 	{
-		std::cin >> tempCoord.y;
-		//PressAnyKey();
-		
+		if (!ReadConsoleInput(
+			Global::hStdin,
+			inputBuffer,
+			128,
+			&numInput)	)
+		{
+			isInMenu = false;
+			std::cout << "\n\nERROR IN READ INPUT";
+		}
+		for (int inpIter = 0; inpIter < numInput; inpIter++)
+		{
+			switch (inputBuffer[inpIter].EventType)
+			{
+			case MOUSE_EVENT:
+				tempCoord = MenuMouseProc(inputBuffer[inpIter].Event.MouseEvent);
+				break;
+			default:
+				break;
+			}
+		}
 		
 		if (tempMenu->getMenu(tempCoord) == nullptr)
 		{
@@ -52,6 +80,25 @@ int main()
 	
 	std::cout << "\n\n\n";
 
+
+	SetConsoleMode(Global::hStdin, oldWindowMode);
 	PressAnyKey();
 	return 0;
+}
+
+Coord MenuMouseProc(MOUSE_EVENT_RECORD mouseEvent)
+{
+	#ifndef MOUSE_HWHEELED
+	#define MOUSE_HWHEELED 0x0008
+	#endif
+	switch (mouseEvent.dwEventFlags)
+	{
+	case 0:
+		if (mouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			return Coord(mouseEvent.dwMousePosition.X, mouseEvent.dwMousePosition.Y);
+		}
+	}
+
+	return Coord(-5, -5);		//Negative coordinates are all treated as not being on any menu.
 }
